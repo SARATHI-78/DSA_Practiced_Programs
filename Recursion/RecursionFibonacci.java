b83af1c67c34ede445c840057aa12623b9d60d13
SARATHI-78 <sundarasarathi78@gmail.com>
2026-09-10 13:45:39 +0530

    Add Recursion\RecursionFibonacci.java

---
+++ Recursion/RecursionFibonacci.java
@@ -0,0 +1,17 @@
+class RecursionFibonacci
+{
+    static int fibonacci(int n)
+    {
+       if(n==0 || n==1)
+       {
+        return n;
+       }
+
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.print(fibonacci(6));
+    }
+}
