5ad9bcce75bc8d27571049e5a0e6d240cdb96e8d
SARATHI-78 <sundarasarathi78@gmail.com>
2026-09-10 13:45:34 +0530

    Add Recursion\RecursionFactorial.java

---
+++ Recursion/RecursionFactorial.java
@@ -0,0 +1,17 @@
+class RecursionFactorial
+{
+    static int factorial(int n)
+    {
+        if(n == 0)
+        {
+            return 1;
+        }
+
+        return n * factorial(n-1);
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.print(factorial(5));
+    }
+}
