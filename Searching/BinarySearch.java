b86adfae524d57461986c45b399cdf83bc60f2ad
SARATHI-78 <sundarasarathi78@gmail.com>
2026-09-10 13:45:58 +0530

    Add Searching\BinarySearch.java

---
+++ Searching/BinarySearch.java
@@ -0,0 +1,40 @@
+class BinarySearch
+{
+    public static void main(String[] args)
+    {
+        int[] arr = {10, 20, 30, 40, 50, 60, 70};
+        int target = 60;
+
+        int left = 0;
+        int right = arr.length - 1;
+        int index = -1;
+
+        while(left <= right)
+        {
+            int mid = (left + right) / 2;
+
+            if(arr[mid] == target)
+            {
+                index = mid;
+                break;
+            }
+            else if(arr[mid] > target)
+            {
+                right = mid - 1;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+
+        if(index != -1)
+        {
+            System.out.print("Element Found at index : " + index);
+        }
+        else
+        {
+            System.out.print("Element Not Found");
+        }
+    }
+}
