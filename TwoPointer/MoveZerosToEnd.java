7ebe8d4a389170c65860ef2cb8b2d285a920ff75
SARATHI-78 <sundarasarathi78@gmail.com>
2026-09-10 13:47:13 +0530

    Add TwoPointer\MoveZerosToEnd.java

---
+++ TwoPointer/MoveZerosToEnd.java
@@ -0,0 +1,26 @@
+class  MoveZerosToEnd
+{
+    public static void main(String[] args)
+    {
+        int[] arr = {0, 1, 0, 3, 12};
+        int left = 0;
+        int right = 0;
+
+        while(right < arr.length)
+        {
+            if(arr[right] != 0)
+            {
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                
+                left++;
+            }
+            right++;
+        }
+        for(int i : arr)
+        {
+            System.out.print(i + " ");
+        }
+    }
+}
