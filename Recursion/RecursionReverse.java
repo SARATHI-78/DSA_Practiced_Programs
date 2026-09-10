a7176f5f6de40d7fb96637593fa6484bbb5000c3
SARATHI-78 <sundarasarathi78@gmail.com>
2026-09-10 13:45:49 +0530

    Add Recursion\RecursionReverse.java

---
+++ Recursion/RecursionReverse.java
@@ -0,0 +1,16 @@
+class RecursionReverse
+{
+    static void reverse(int n)
+    {
+        if(n == 0)
+        {
+            return;
+        }
+        System.out.print(n + " ");
+        reverse(n-1);
+    }
+    public static void main(String[] args)
+    {
+        reverse(5);
+    }
+}
