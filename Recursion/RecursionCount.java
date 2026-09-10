dce3377b96af3ac106fd1dc41555632ce6117a47
SARATHI-78 <sundarasarathi78@gmail.com>
2026-09-10 13:45:29 +0530

    Add Recursion\RecursionCount.java

---
+++ Recursion/RecursionCount.java
@@ -0,0 +1,18 @@
+class RecursionCount
+{
+    static void countDown(int n)
+    {
+        if(n == 0)
+        {
+            return;
+        }
+
+        System.out.println(n);
+        countDown(n-1);
+    }
+
+    public static void main(String[] args)
+    {
+        countDown(5);
+    }
+}
