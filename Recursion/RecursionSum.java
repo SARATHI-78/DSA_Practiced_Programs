4ba7758177425f2d487beaabe40242c42a3fcce0
SARATHI-78 <sundarasarathi78@gmail.com>
2026-09-10 13:45:53 +0530

    Add Recursion\RecursionSum.java

---
+++ Recursion/RecursionSum.java
@@ -0,0 +1,17 @@
+class RecursionSum
+{
+    static int sum(int n)
+    {
+        if(n == 0)
+        {
+            return 0;
+        }
+
+        return n + sum(n-1);
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.print(sum(5));
+    }
+}
